272df58b8ae247eabd08ab328e9d36bee28b7593
IvanTymoshchuk <timohin2231@gmail.com>
2023-04-18 09:34:52 +0000

    Deploying to gh-pages from @ IvanTymoshchuk/goit-js-hw-11@e427cd4103fc64fc35c8f7135af6d7daa12e8227 🚀

---
+++ index.7066277e.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector(".header"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.7066277e.js.map
